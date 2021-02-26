8e7c47558ac5841b9212bc514337c1f7a92aba55
leandroturrion <leandrourielturrion@hotmail.com>
2021-02-26 06:12:49 -0300

    react push2

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,29 @@
 import React from "react"
+import imagenDefault from "../images/exercise.png"
+import "./style/Card.css"
+
 
 class Card extends React.Component{
         render(){
-        return <h1>Card pERROO</h1>
+        return (
+            
+            <div className="card mx-auto Fitness-Card">
+                <div className="card-body">
+                    <div className="row center">
+
+                       <div className="col-6">
+                      <img src={imagenDefault} className="float-rigth" />
+                       </div>
+                        <div className="col-6 Fitness-Card-Info">
+                        <h1>Titulo 1</h1> 
+                         <p>parrafo piloto loremdigmosle</p> 
+                      </div>
+
+                    </div>
+             </div>
+
+            </div>
+        )
     }
 
 }
